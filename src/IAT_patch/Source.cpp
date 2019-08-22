c20ea484e18ef5b3126708c5768ab104b8fc25fa
Boudewijn meijer <bcm.meijer@gmail.com>
2019-08-22 15:12:30 +0200

    changed to class, added restore

--- src/IAT_patch/Source.cpp
+++ src/IAT_patch/Source.cpp
@@ -1,57 +1,16 @@
-#include <Windows.h>
-#include <dbghelp.h>
-#include <iostream>
-#include <assert.h>
-#pragma comment(lib, "Dbghelp.lib")
+#include "IATHOOK.h"
 
-BOOL PatchIAT(PVOID pModuleBase, const char* szTargetModuleName, const char* szFunction, PVOID targetFunction);
 int MyMessageBoxA(HWND hWnd, LPCSTR lpText, LPCSTR lpCaption, UINT uType);
 
-
 int main() {
-	
-	if (PatchIAT(GetModuleHandle(NULL), "USER32.dll", "MessageBoxA", MyMessageBoxA)) {
-		std::cout << "[+] Successfully patched IAT\n";
+	IATHOOK hook(GetModuleHandle(NULL), (char*)"USER32.dll", (char*)"MessageBoxA", MyMessageBoxA);
+	if (hook.PatchIAT()) {
 		MessageBoxA(NULL, "test", "test", MB_OK);
-	}
-	else
-		std::cout << "[-] Could not patch IAT\n";
-	return 0;
-}
-
-BOOL PatchIAT(PVOID pModuleBase, const char* szTargetModuleName, const char* szFunction, PVOID targetFunction) {
-	ULONG size;
-	PIMAGE_IMPORT_DESCRIPTOR importDir = (PIMAGE_IMPORT_DESCRIPTOR)ImageDirectoryEntryToDataEx(pModuleBase, TRUE, IMAGE_DIRECTORY_ENTRY_IMPORT, &size, NULL);
-	if (!importDir)
-		return FALSE;
-	PVOID orininalFunction = GetProcAddress(GetModuleHandle(szTargetModuleName), szFunction);
-	if (!orininalFunction)
-		return FALSE;
-
-	while (importDir->Name) {
-		char* moduleName = (char*)((uint64_t)pModuleBase + importDir->Name);
-		if (strcmp(moduleName, szTargetModuleName) == 0) {
-			PIMAGE_THUNK_DATA pIAT = (PIMAGE_THUNK_DATA)((uint64_t)pModuleBase + importDir->FirstThunk);
-			if (pIAT) {
-				while (pIAT->u1.Function) {
-					PROC* funcStorage = (PROC*)&pIAT->u1.Function;
-					if (*funcStorage == orininalFunction) {
-						MEMORY_BASIC_INFORMATION meminfo;
-						VirtualQuery(funcStorage, &meminfo, sizeof(MEMORY_BASIC_INFORMATION));
-						if (!VirtualProtect(meminfo.BaseAddress, meminfo.RegionSize, PAGE_READWRITE, &meminfo.Protect))
-							return FALSE;
-						*funcStorage = (PROC)targetFunction;
-						DWORD dwOldProtect;
-						VirtualProtect(meminfo.BaseAddress, meminfo.RegionSize, meminfo.Protect, &dwOldProtect);
-						return TRUE;
-					}
-					pIAT++;
-				}
-			}
+		if (hook.RestoreIAT()) {
+			MessageBoxA(NULL, "test", "test", MB_OK);
 		}
-		importDir++;
 	}
-	return FALSE;
+	return 0;
 }
 
 int MyMessageBoxA(HWND hWnd, LPCSTR lpText, LPCSTR lpCaption, UINT uType) {
